d97c948321f6dfff0bcc1622d94ea791a3ca444e
riti12345 <riti6266@gmail.com>
2015-11-19 22:22:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function()
+{
+ $('#save').click(function(){
+     
+     var title=$('#title').val();
+     var date=$('#date').val();
+     var detail=$('#detail').val();
+
+     $('#title-of-todo').append($('#month-date-year').append("<li>" + "<b>"+ title + " 		"+ "<td>" + date + "</td>" +"</li>"));
+    
+
+     event.preventDefault();
+
+
+ }),
+
+ $('#detail-1').click(function(){
+ 	 var title=$('#title').val();
+     var date=$('#date').val();
+     var detail=$('#detail').val();
+
+ 	$('#footer').append($('#title-date-detail').append("<li>" + "<b>" + title + "</br>" + date + "</b>"+ "</br>" +detail + "</li>" ));
+ 	event.preventDefault();
+
+ }),
+ $('#delete').click(function(){
+ 	 var title=$('#title').val();
+     var date=$('#date').val();
+     var detail=$('#detail').val();
+
+    $("ol").remove();
+
+
+
+ });
+ return;
+});
+
